cfde75f2f0807e762a741af5e75c42139cd9fbb4
Daniel Novotny <svc_OKC_BT204@BT228-10>
2018-01-31 20:32:45 -0600

    with rnd

--- CIS2013_Week03_Lab01/flowControl.cpp
+++ CIS2013_Week03_Lab01/flowControl.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
+#include<stdlib.h>
+#include<time.h>
 using namespace std;
 
 int main() {
-	char continueProgram = 'y';
+
+
+	srand(time(NULL));
+	char cont;
+
+	int num1 = rand() % 10 + 1;
+	cout << "your random number is " << num1;
+	cin >> cont;
+
+
+
+
+
+
+	/*char continueProgram = 'x';
 	
+	for (int i = 0; i <= 99; i = i++) {
+		cout << i << "bottles of beer on the wall" << endl;
+	}
 
 	do{
 
@@ -22,7 +41,7 @@ int main() {
 		}
 	} while (continueProgram == 'y');
 
-
+*/
 
 	return 0;
 }
