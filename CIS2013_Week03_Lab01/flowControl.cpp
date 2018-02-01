df89ba1e7779bcb6866afadf811e337a108fb5e4
Daniel Novotny <svc_okc_bt204@ad.osuokc.edu>
2018-01-31 20:13:46 -0600

    lab1 week3

--- CIS2013_Week03_Lab01/flowControl.cpp
+++ CIS2013_Week03_Lab01/flowControl.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main() {
+	char continueProgram = 'y';
+	
 
+	do{
+
+		cout << "Do you want to continue (y/n)." << endl;
+		cin >> continueProgram;
+
+		switch (continueProgram) {
+		case 'y':
+			cout << "going to continue" << endl;
+			break;
+		case 'n':
+			cout << "going to exit" << endl;
+			break;
+		default:
+			continueProgram = 'y';
+		}
+	} while (continueProgram == 'y');
+
+
+
+	return 0;
 }
